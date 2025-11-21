754f401e73fa1d963f2b0143d3277a1fea30a016
jayl <jayl>
2025-11-21 11:25:34 +0000

    Added clockout validation

--- modules.py
+++ modules.py
@@ -94,46 +94,74 @@ def load_alarm_time(file_path=config_file):
         data = json.load(f)
     return data.get("alarm_time", "00:00")
 
+def is_clockout_in_range(start, end, current):
+    current = str(current)
+    start = str(start)
+    end = str(end)
+
+    if start <= end:
+        return start <= current <= end
+    else:
+        return start <= current or current <= end
+
 def clockout_action():
     date_of_alarm = None
-    clockout_completed = 1
+    clockout_completed = None
 
-    alarm_time = load_alarm_time()
+    allowed_time_before_amount = 4 # Hours
+
+    alarm_time, clockout_time = get_config()
+
     alarm_time = datetime.strptime(alarm_time, "%H:%M").time()
+    clockout_time_full = datetime.strptime(clockout_time, "%H:%M")
+    clockout_time = datetime.strptime(clockout_time, "%H:%M").time()
+    
+    current_time_full = datetime.now()
+    current_time_only = datetime.strptime(current_time_full.strftime("%H:%M"), "%H:%M").time()
     current_time = datetime.now().time()
 
-    if current_time < alarm_time:
-        date_of_alarm = date.today()
+    allowed_time_before = (clockout_time_full - timedelta(hours=allowed_time_before_amount)).time()
+    allowed_time_before = datetime.strptime(allowed_time_before.strftime("%H:%M"), "%H:%M").time()
+
+    clockout_completed = is_clockout_in_range(allowed_time_before, clockout_time, current_time_only)
+
+    if clockout_completed:
+
+        if current_time < alarm_time:
+            date_of_alarm = date.today()
+        else:
+            date_of_alarm = date.today() + timedelta(days=1)
+
+        conn = sqlite3.connect(db_file)
+        cur = conn.cursor()
+
+        cur.execute("SELECT streak FROM history ORDER BY id DESC LIMIT 1")
+        row = cur.fetchone()
+
+        if row is None:
+            previous_streak = 0  # no previous entries
+        else:    
+            previous_streak = row[0]
+        new_streak = previous_streak + 1
+
+        data = [date_of_alarm, clockout_completed, new_streak]
+
+        try:
+            cur.execute("""
+                INSERT INTO history (date, clockout, streak)
+                VALUES (?, ?, ?)
+            """, data)
+
+            conn.commit()
+
+        except sqlite3.IntegrityError as e:
+            print("Error inserting clockout data:", e)
+
+        cur.close()
+        conn.close()
+
     else:
-        date_of_alarm = date.today() + timedelta(days=1)
-
-    conn = sqlite3.connect(db_file)
-    cur = conn.cursor()
-
-    cur.execute("SELECT streak FROM history ORDER BY id DESC LIMIT 1")
-    row = cur.fetchone()
-
-    if row is None:
-        previous_streak = 0  # no previous entries
-    else:    
-        previous_streak = row[0]
-    new_streak = previous_streak + 1
-
-    data = [date_of_alarm, clockout_completed, new_streak]
-
-    try:
-        cur.execute("""
-            INSERT INTO history (date, clockout, streak)
-            VALUES (?, ?, ?)
-        """, data)
-
-        conn.commit()
-
-    except sqlite3.IntegrityError as e:
-        print("Error inserting clockout data:", e)
-
-    cur.close()
-    conn.close()
+        print("Clockout action not in allowed time range.")
 
     return
 
