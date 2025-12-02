dbc4f1aef6ea1c9300461f3ca8259fb3206fc153
jayl <jayl>
2025-12-02 02:38:40 +0000

    Simplified default config

--- config_setup.py
+++ config_setup.py
@@ -17,17 +17,9 @@ def setup_config(file_path=config_file):
         "friday": True,
         "saturday": True,
         "sunday": True
-    },
-    "clockout_days": {
-        "monday": True,
-        "tuesday": True,
-        "wednesday": True,
-        "thursday": True,
-        "friday": True,
-        "saturday": True,
-        "sunday": True
     }
 }
+    
     try:
         with open(file_path, 'x') as file:
             json.dump(defaults, file, indent=4)
