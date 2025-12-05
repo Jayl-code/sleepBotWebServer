ab273b917bd90baf60a16b78727129b584e0c364
jayl <jayl>
2025-12-05 10:42:16 +0000

    Stopped alarm sounding on days it shouldnt

--- modules/get_config.py
+++ modules/get_config.py
@@ -1,6 +1,6 @@
 # Imports
 import json
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
 # File paths
 config_file = 'config.json'
@@ -27,3 +27,14 @@ def get_clockout_time(file_path=config_file):
 def get_last_required_day():
     #todo update to get the date of the last needed day
     return date.today() - timedelta(days=1)
+
+def alarm_today(file_path=config_file):
+    todays_day =  datetime.today().strftime("%A").lower()
+
+    with open(file_path, 'r') as file:
+        content = file.read().strip()
+        data = json.loads(content)
+    
+    active_today = data["alarm_days"][todays_day]
+
+    return active_today
