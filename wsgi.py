b7d9bd3bd60141b302b95ededa182e4a10064405
jayl <jayl>
2026-01-20 12:32:32 +0000

    Again

--- wsgi.py
+++ wsgi.py
@@ -15,5 +15,5 @@ log = logging.getLogger(__name__)
 
 # Main starting point for gunicorn 
 if __name__ == "__main__":
+    log.info("Starting WSGI server...")
     app.run()
-    log.info("WSGI server started")
